b95d87f0b5d0118a31205d4f7ff9f3fde3dfc982
belodpav <belod.pav@gmail.com>
2018-03-31 03:48:17 +0300

    (test) Для тестирования функции шаблонизатора

---
+++ test/src/template/template.test.js
@@ -0,0 +1,54 @@
+const {assert} = require('chai');
+const {genCodeWithData} = require('../../../src/template/template');
+
+describe('flux-dron/templating', () => {
+    it('Подставляет значение переменной в шаблон', () => {
+        const srcTmp = `
+            <div class="block">
+                <h2 class="block__title">{{title}}</h2>
+                <span>This is a block</span>
+            </div>
+        `.trim();
+        const expectedResult = `
+            <div class="block">
+                <h2 class="block__title">World</h2>
+                <span>This is a block</span>
+            </div>
+        `.trim();
+
+        const data = {
+            title: 'World'
+        };
+
+        const htmlResult = genCodeWithData(srcTmp, data);
+
+        assert.equal(expectedResult, htmlResult);
+    });
+
+    it('Подставляет значение переменной в шаблон'
+        + ' при разной глубине объекта с данными', () => {
+        const srcTmp = `
+            <div class="block">
+                <h2 class="block__title">{{block.title}}</h2>
+                <span>Number {{number}}</span>
+            </div>
+        `.trim();
+        const expectedResult = `
+            <div class="block">
+                <h2 class="block__title">World</h2>
+                <span>Number 42</span>
+            </div>
+        `.trim();
+
+        const data = {
+            block: {
+                title: 'World'
+            },
+            number: 42
+        };
+
+        const htmlResult = genCodeWithData(srcTmp, data);
+
+        assert.equal(expectedResult, htmlResult);
+    });
+});
