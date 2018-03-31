a0b45e30f8fe49f2b134b58f9e6d39c295681380
belodpav <belod.pav@gmail.com>
2018-03-31 18:09:43 +0300

    (new) Для объеденения сущностей фреймворка
    Компоненты фреймворка объеденяются в единый обьект
    для удобства работы с фреймворком и обращения к его компонентам.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+const Dispatcher = require('core/dispatcher');
+const Store = require('core/store');
+const View = require('core/view');
 
+const FluxDrone = {
+    Store: Store,
+    Dispatcher: Dispatcher,
+    View: View
+};
 
+module.exports = FluxDrone;
