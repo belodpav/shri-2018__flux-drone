07d9d29310e7ef01b390221d83bd93419afb1462
belodpav <belod.pav@gmail.com>
2018-04-01 16:54:14 +0300

    (feature) Для расширения возможностей
    Теперь Представление обновляется только в тех элементах где необходимо
    это изменение.

--- src/core/view.js
+++ src/core/view.js
@@ -1,4 +1,5 @@
-const {genCodeWithData} = require('../template/template');
+const {tempEngine} = require('../template/template');
+const {getID} = require('../helpers');
 
 /**
  * Представляет View сущность
@@ -18,8 +19,9 @@ class View {
         this._events = events;
 
         this._node = document.querySelector(root);
+        this._srcNode = document.createElement('div');
 
-        this.update();
+        this._init();
     }
 
     // public methods
@@ -28,22 +30,126 @@ class View {
      * Обновляет состояние Представления
      * основываясь на данных Хранилища
      * за которым наблюдает Представление
+     * Обновляем только те элементы, которые
+     * завязаны на получение данных из хранилища
      */
     update() {
-        const data = this._getData();
+        const data = Object.assign({}, this._getData());
+        const keys = Object.keys(data);
 
-        const html = genCodeWithData(this._temp, data);
-        this._node.innerHTML = html;
-        this._initEvents();
+        keys
+        .filter((key) => data[key] !== this._curState[key])
+        .forEach((key) => this._updateNodeAll(key, data[key]));
+
+        this._curState = Object.assign({}, data);
     }
+
 
     // private methods
 
     /**
-     * Инициализирует события для DOM элементов
+     * Инициализирует Представление
      */
-    _initEvents() {
-        const activeElements = this._node.querySelectorAll('[data-action]');
+    _init() {
+        const data = this._getData();
+
+        this._srcNode.innerHTML = this._temp;
+        this._node.innerHTML = tempEngine(this._temp, data);
+
+        this._curState = Object.assign({}, data);
+
+        this._initEvents(this._node);
+        this._buildDepsTree();
+    }
+
+    /**
+     * Обновляет все DOM элементы
+     * зависимые от ключа key
+     * @param {String} key
+     * @param {*} val
+     */
+    _updateNodeAll(key, val) {
+        const elements = this._depsTree;
+
+        elements.forEach((el) => {
+            if (el.keys.indexOf(key) !== -1) {
+                const parent = el.parent;
+                const newNode = this._updateNode(el.temp);
+
+                el.real = this._replaceNode(el.real, newNode);
+
+                this._initEvents(parent);
+            }
+        });
+    }
+
+    /**
+     * Возвращает новый Node
+     * построенный на шаблоне
+     * но еще не вставленный в DOM
+     * @param {Node} el
+     * @return {Node}
+     */
+    _updateNode(el) {
+        const node = {
+            tempHTML: el.outerHTML,
+            wrap: document.createElement('div')
+        };
+
+        node.wrap.innerHTML = tempEngine(node.tempHTML, this._getData());
+
+        return node.wrap.firstChild;
+    }
+
+    /**
+     *  Возвращает ссылку на обновленный DOM
+     *  элемент
+     * @param {Node} oldNode
+     * @param {Node} newNode
+     * @return {Node}
+     */
+    _replaceNode(oldNode, newNode) {
+        const id = getID();
+        newNode.setAttribute('data-key', id);
+
+        oldNode.replaceWith(newNode);
+
+        const newRealNode = this._node.querySelector('[data-key="' + id + '"]');
+
+        newRealNode.removeAttribute('data-key');
+
+        return newRealNode;
+    }
+
+    /**
+     *  Строит дерево зависимостей DOM
+     *  эелементов от полей состояния хранилища
+     */
+    _buildDepsTree() {
+        const depsTree = [];
+        const depsTemp = this._srcNode.querySelectorAll('[data-deps]');
+        const deps = this._node.querySelectorAll('[data-deps]');
+
+        deps.forEach((el, i) => {
+            const keys = el.getAttribute('data-deps').split(',');
+
+            depsTree.push({
+                keys: keys.map((key) => key.trim()),
+                real: el,
+                temp: depsTemp[i]
+            });
+        });
+
+        this._depsTree = depsTree;
+    }
+    /**
+     * Инициализирует события для DOM элементов
+     * внутри элемента elem
+     * @param {Node} elem
+     */
+    _initEvents(elem) {
+        if (!elem) return;
+        const activeElements = elem.querySelectorAll('[data-action]');
 
         activeElements.forEach((el) => {
             const actionPairs = el.getAttribute('data-action').split(',');
